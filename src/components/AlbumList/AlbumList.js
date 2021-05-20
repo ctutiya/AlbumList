4ed10b27f0a004758fc0f9cacaeb0b9e11fc04ac
Carlos Tutiya <ctutiya@hotmail.com>
2021-05-20 16:06:07 -0700

    initial commit

---
+++ src/components/AlbumList/AlbumList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class AlbumList extends React.Component {
+    render() {
+        return (
+            <>
+                Hello!
+            </>
+        )
+    }
+
+}
+
+export default AlbumList
